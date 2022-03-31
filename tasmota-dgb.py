ebe48bf498fe42092e1bb44013234434865a4cd2
Tediore <tediore.maliwan@gmail.com>
2022-03-31 08:10:23 -0500

    3

--- tasmota-dgb.py
+++ tasmota-dgb.py
@@ -69,19 +69,16 @@ def on_message(client, userdata, msg):
         z = 1 - x - y
         xyz_color = XYZColor(x, y, z, 10)
         rgb_color = convert_color(xyz_color, AdobeRGBColor)
-        r = '%02x' % int(rgb_color.clamped_rgb_r * 255)
-        g = '%02x' % int(rgb_color.clamped_rgb_g * 255)
-        b = '%02x' % int(rgb_color.clamped_rgb_b * 255)
-        dg_payload = ('TASMOTA_DGRtasmota_test\x00\x11\x00\x00\x00\xe0\x06' + '\\x' + r + '\\x' + g + '\\x' + b + '\x00\x00\x0b\x00').encode()
-        print(dg_payload)
-        d.devgroup_publisher(dg_payload)
+        rgb_color = '#%02x%02x%02x' % (int(rgb_color.clamped_rgb_r * 255), int(rgb_color.clamped_rgb_g * 255), int(rgb_color.clamped_rgb_b * 255))
+        print(rgb_color)
+        d.devgroup_publisher(rgb_color)
 
 logging.basicConfig(level='INFO', format='%(asctime)s %(levelname)s: %(message)s')
 mqtt_connect()
 
 class DeviceGroup:
     def __init__(self):
-        pass
+        self.throttle = 0
 
     def devgroup_listener(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -92,8 +89,8 @@ class DeviceGroup:
         self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     def devgroup_publisher(self, payload):
-        self.sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        self.sock_send.sendto(payload, (dg_addr, dg_port))
+        client.publish('cmnd/tasmota_test/color', payload)
+        self.throttle = 1
 
     def devgroup_converter(self):
         while True:
@@ -103,25 +100,29 @@ class DeviceGroup:
                     print(data)
                     # print(data[data.find(b'\x00')+4:])
                     if b'\xe0' in data:
-                        # tasmota color / color temp command
-                        info = data[data.find(b'\xe0'):]
-                        info = binascii.hexlify(info).decode()
-                        rgb = info[4:10]
-                        ct = info[10:14]
-                        cw = int(info[10:12], 16)
-                        ww = int(info[12:14], 16)
-                        mired = int(327 - (173 * (cw / 255)) + (173 * (ww / 255)))
-                        if rgb != '000000':
-                            payload = json.dumps({'color': {'hex': f'#{rgb}'}})
+                        if self.throttle:
+                            self.throttle = 0
                         else:
-                            payload = json.dumps({'color_temp': mired})
-                        client.publish(f'zigbee2mqtt/{zigbee2mqtt[group]}/set', payload)
+                            # tasmota color / color temp command
+                            info = data[data.find(b'\xe0'):]
+                            info = binascii.hexlify(info).decode()
+                            rgb = info[4:10]
+                            ct = info[10:14]
+                            cw = int(info[10:12], 16)
+                            ww = int(info[12:14], 16)
+                            mired = int(327 - (173 * (cw / 255)) + (173 * (ww / 255)))
+                            if rgb != '000000':
+                                payload = json.dumps({'color': {'hex': f'#{rgb}'}})
+                            else:
+                                payload = json.dumps({'color_temp': mired})
+                            client.publish(f'zigbee2mqtt/{zigbee2mqtt[group]}/set', payload)
 
                     if b'\x80' in data:
                         # tasmota power command
                         info = data[data.find(b'\x80'):]
                         info = binascii.hexlify(info).decode()
                         pwr = int(info[2:4])
+                        print(info)
                         if pwr in [0, 1]:
                             client.publish(f'zigbee2mqtt/{zigbee2mqtt[group]}/set', json.dumps({'state': power[pwr]}))
 
